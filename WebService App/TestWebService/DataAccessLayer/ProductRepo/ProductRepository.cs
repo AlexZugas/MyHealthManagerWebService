dae7fb58022ce2aa04c2828e4ebdfa8198515dbc
Daniel Vazquez <daniel.vzz@hotmail.com>
2016-11-19 10:30:18 -0600

    ADD Product Repository

---
+++ WebService App/TestWebService/DataAccessLayer/ProductRepo/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Model;
+
+namespace DataAccessLayer
+{
+    public class ProductRepository
+    {
+        MyHealthManagerEntities Context;
+
+        public ProductRepository()
+        {
+            Context = new MyHealthManagerEntities();
+        }
+
+        public Product get_Product(string Code)
+        {
+            var product = (from tproduct in Context.Products
+                          where tproduct.Code == Code
+                          select tproduct).FirstOrDefault();
+
+            return product;
+        }
+    }
+}
