70b59ca00e0b4df447a629f0fcb5584c84694ff8
Daniel Vazquez <daniel.vzz@hotmail.com>
2016-11-19 10:16:19 -0600

    ADD ProductLogic

---
+++ WebService App/TestWebService/BusinessLogicLayer/ProductLogic/ProductLogic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Model;
+
+namespace BusinessLogicLayer
+{
+    public class ProductLogic
+    {
+        ProductRepository productBLL = new ProductRepository();
+
+        public Product get_ProductByCode(string Code)
+        {
+            Product product = new Product();
+            product = productBLL.get_Product(Code);
+            return product;
+        }
+    }
+}
